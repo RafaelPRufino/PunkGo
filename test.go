ea85e6bc4082a0014bf32de786770a77d14df4fd
Rafael Rufino <rafaelrufino14@hotmail.com>
2021-07-25 00:11:27 -0300

    Update test.go

--- test.go
+++ test.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"fmt"
-	"github.com/RafaelPRufino/PunkGo/integration/google/auth/option"
+	option "github.com/RafaelPRufino/PunkGo/integration/google/auth/option"
 	google "github.com/RafaelPRufino/PunkGo/integration/google/client"
 )
 
 func main() {
+	// New client google with Google Credentials Application
 	client := google.NewClient(option.WithGoogleCredentialsFile("keys/google-keys.json"))
 
+	//Authenticate in Google API with User Credentials
 	client.SignIn(option.WithOAuthCredentialsFile("keys/access-token.json"))
 
+	//Create new Analytics Service
 	ga := client.NewAnalyticsService()
-	request := ga.CreateRequest("230920570")
+	//Create new Request Analytics Service with ViewId
+	request := ga.CreateRequest("555555555")
+	//  Add Dimensions Reports
 	request.AddDimension("ga:browser")
+	//  Add Metrics Reports
 	request.AddMetric("ga:users")
 	request.AddMetric("ga:newUsers")
 	request.AddMetric("ga:newUsers")
+	//Process report
 	response, err := ga.Do(request)
 
-	fmt.Println(response, err)
-
+	if err != nil {
+		fmt.Println("Error:" +" "+ err.Error())
+	} else {
+		//Print Data JSON
+		json, err:=response.MarshalJSON()
+		fmt.Println(string(json) , err)
+	}
 }
